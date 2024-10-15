2d5afa9bda4a33d55af79efd2b565eb2b53c7303
tmtri3102 <tmtri310251@gmail.com>
2024-10-15 16:32:34 +0700

    new student class

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,12 @@
-package PACKAGE_NAME;
-
 public class Student {
+    private String name = "John";
+    private String classes = "C02";
+    Student(){};
+
+    private void setName(String name) {
+        this.name = name;
+    }
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class Test {
+    private static String name = "John";
+    private static void setName(String newName) {
+        name = newName;
+        System.out.println(name);
+    }
+    public static void main(String[] args) {
+        Student s = new Student();
+        setName("Tommy");
+
+        s.setClasses("C08");
+    }
 }
